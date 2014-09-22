65258405f35c1c0078a7a64cb489eaa6a99ed802
Alex Jurkiewicz <alex.jurkiewicz@siteminder.com>
2014-09-22 22:58:35 +1000

    check_python: remove another pylint/pyflake coverage duplication

--- pender-scripts/check_python.py
+++ pender-scripts/check_python.py
@@ -37,8 +37,8 @@ def check_pylint(temp_file):
     '''
     PyLint
     Exclude:
-      - C0301: line too long (covered by pyflake E501)
-      - C0330: Wrong hanging indentation. (covered by pyflake E126)
+      - C0301: line too long (reported better by pyflake E501)
+      - C0330: Wrong hanging indentation. (reported better by pyflake E126)
     '''
     try:
         pylint_cmdline = ['pylint',
@@ -63,11 +63,12 @@ def check_pyflake(temp_file):
       - E225 missing whitespace around operator (pylint C0326)
       - F821 undefined name (pylint E0602)
       - F841 local variable '...' is assigned to but never used (pylint W0612)
+      - E701 multiple statements on one line (colon) (pylint C0321)
     '''
     try:
         subprocess.check_output(['flake8',
                                  '--max-line-length={}'.format(MAX_LINE_LEN),
-                                 '--ignore=E225,F821',
+                                 '--ignore=E225,F821,E701',
                                  '--format=%(row)3d: %(text)s (%(code)s)',
                                  temp_file])
     except subprocess.CalledProcessError as err:
