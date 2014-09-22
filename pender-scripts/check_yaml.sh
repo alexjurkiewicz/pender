7182148e03deb6da2227e2959b4d0c984828cf0a
Alex Jurkiewicz <alex.jurkiewicz@siteminder.com>
2014-09-22 22:21:46 +1000

    check_yaml: shorten error message

--- pender-scripts/check_yaml.sh
+++ pender-scripts/check_yaml.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 REAL_FILE=$1
-#TEMP_FILE=$2
+TEMP_FILE=$2
 #FILE_MIME=$3
 
-if [[ $REAL_FILE != '*.yaml' ]] ; then
+if [[ $REAL_FILE != *.yaml ]] ; then
         exit 0
 fi
 
-python -c "import yaml; yaml.load(open('$testfile'))"
+python -c "import yaml; yaml.load(open('$TEMP_FILE'))" 2>&1 | awk '/^yaml/ {p=1} p;'
 
 exit 0
