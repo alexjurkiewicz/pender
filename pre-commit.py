5544501e6cc8ed06f1df1bf1b16bc6513d86ece1
Alex Jurkiewicz <alex@jurkiewi.cz>
2015-04-17 17:09:57 +1000

    py2.6 compat

--- pre-commit.py
+++ pre-commit.py
@@ -30,6 +30,31 @@ GIT_EXIT_OK = 0
 GIT_EXIT_VETO = 1
 
 
+def check_output(*popenargs, **kwargs):
+    r"""Run command with arguments and return its output as a byte string.
+
+    Backported from Python 2.7 as it's implemented as pure python on stdlib.
+
+    >>> check_output(['/usr/bin/python', '--version'])
+    Python 2.6.2
+    """
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, _ = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        error = subprocess.CalledProcessError(retcode, cmd)
+        error.output = output
+        raise error
+    return output
+
+
+if not hasattr(subprocess, 'check_output'):
+    subprocess.check_output = check_output
+
+
 class PenderError(Exception):
     """Simple wrapper."""
 
